75e48b8f931431a0ce23635e87d2b744de5e4e8f
Mark Kennedy <mwkennedy54@gmail.com>
2025-03-26 15:56:48 -0700

    Update App.js
    Fixed backend layout descriptor for App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { connect } from "react-redux"
 import { authProtectedRoutes, publicRoutes } from "./routes/"
 import AppRoute from "./routes/route"
 
-// layouts
+// Layouts
 import VerticalLayout from "./components/VerticalLayout/"
 import HorizontalLayout from "./components/HorizontalLayout/"
 import NonAuthLayout from "./components/NonAuthLayout"
